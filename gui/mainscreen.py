1c9948b6db8760e96ca75647e81cc4bf4e039cb6
Gabor Cseh <csega@mailbox.hu>
2022-06-14 09:44:07 +0200

    PEP8 compliance.

--- gui/mainscreen.py
+++ gui/mainscreen.py
@@ -1,20 +1,26 @@
+from os import system as runcmd
+import threading
+import json
+from tkinter import messagebox
+import tkinter as tk
+import openbubi
 import sys
 sys.path.append("../")
-import openbubi
-import tkinter as tk
-from tkinter import messagebox
-import json
-import threading
-from os import system as runcmd
 
 map = openbubi.BubiMap()
 user = openbubi.BubiUser(sys.argv[1], sys.argv[2])
 
 selectedBike = 0
+
+
 def rentBike():
     user.rentBike(selectedBike)
+
+
 def selectBike(selection):
     selectedBike = selection
+
+
 def enterLocation():
     nameOfNearestStation = map.getNearestStationByAddress(locationField.get())
     messagebox.showinfo("Legközelebbi állomás", nameOfNearestStation)
@@ -25,12 +31,16 @@ def enterLocation():
     for i in json.loads(bikes):
         bikeNumbers.append(i["number"])
     try:
-        bikeList = tk.OptionMenu(root, defaultValue, *bikeNumbers, command=selectBike)
+        bikeList = tk.OptionMenu(
+            root, defaultValue, *bikeNumbers, command=selectBike)
         bikeList.pack()
         rentBTN = tk.Button(root, text="Bicikli bérlése", command="rentBike")
         rentBTN.pack()
     except:
-        messagebox.showerror("Hiba!", "Nincs bicikli a beírt lokációhoz legközelebbi állomáson")
+        messagebox.showerror(
+            "Hiba!", "Nincs bicikli a beírt lokációhoz legközelebbi állomáson")
+
+
 root = tk.Tk()
 root.title("Főképernyő")
 root.geometry("500x200")
